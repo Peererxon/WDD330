bd28f33a9e227feb523c9adf469ab835704674a8
anderson gil <andersongil.dev@gmail.com>
2023-01-14 15:47:23 -0400

    fix broken cart

--- src/js/cart.js
+++ src/js/cart.js
@@ -3,7 +3,7 @@ import { getLocalStorage } from './utils.mjs';
 function renderCartContents() {
   const cartItems = getLocalStorage('so-cart');
 
-  const htmlItems = cartItemTemplate(cartItems);
+  const htmlItems = cartItems.map((item) => cartItemTemplate(item));
 
   document.querySelector('.product-list').innerHTML = htmlItems;
 }
