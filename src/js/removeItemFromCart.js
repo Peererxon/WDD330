ebc1031f3f4bbeb8d27915370b726f7e288e256c
anderson gil <andersongil.dev@gmail.com>
2023-01-16 11:39:08 -0400

    remove multiples items from cart

--- src/js/removeItemFromCart.js
+++ src/js/removeItemFromCart.js
@@ -1,31 +1,32 @@
 import { setLocalStorage, getLocalStorage } from './utils.mjs';
 import { reRenderCartContents } from './cart';
+
+const addRemoveEvent = () => {
+  // remove listener to Add to Cart button
+  const removeButtons = document.querySelectorAll('.removeFromCart');
+
+  removeButtons.forEach((button) => {
+    button.addEventListener('click', removeCartHandler);
+  });
+};
+
 // add to cart button event handler
 async function removeCartHandler(e) {
   const itemId = e.target.getAttribute('data-id');
 
   const items = getLocalStorage('so-cart');
 
-  // if (!items) {
-  //   const item = [];
-
-  //   item.push(product);
-
-  //   setLocalStorage('so-cart', item);
-
-  //   return;
-  // }
+  if (!items) {
+    return;
+  }
 
   const newItems = items.filter((item) => item.Id !== itemId);
 
   setLocalStorage('so-cart', newItems);
 
   reRenderCartContents();
+
+  addRemoveEvent();
 }
 
-// remove listener to Add to Cart button
-const removeButtons = document.querySelectorAll('.removeFromCart');
-
-removeButtons.forEach((button) => {
-  button.addEventListener('click', removeCartHandler);
-});
+addRemoveEvent();
