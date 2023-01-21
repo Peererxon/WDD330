e1ff201e16c0da1679d0548abd4a803384b84153
turtlesboulder <lturtlesrock@gmail.com>
2023-01-20 19:01:40 -0600

    Guideline for how to complete activity

---
+++ src/js/productDetails.js
@@ -0,0 +1,7 @@
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const tentName = urlParams.get('tent')
+
+//fetch json
+
+//plug in all the values according to the json
