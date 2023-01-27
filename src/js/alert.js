fc9f4f3c9269dcfd03a2cc26dcb1eb2da5d791c4
turtlesboulder <lturtlesrock@gmail.com>
2023-01-27 12:23:50 -0600

    Make the alert appear on screen, with an example alert given

--- src/js/alert.js
+++ src/js/alert.js
@@ -1,22 +1,24 @@
-export default class Alert {
+import data from "/json/alerts.js";
+
+class Alert {
     init() {
-      fetch("/json/alerts.json")
-        .then((Response) => Response.json())
-        .then((data) => {
-          console.log(data);
-        });
+      this.data = data;
     }
   
-    buildAlert(alerts) {
-      let section = `<section class="alert-list">`;
-      if (alerts.length > 0) {
-        const elements = alerts.map((alert) => {
-          let p = `<p style="background-color:${alert.background};color:${alert.color}">${alert.message}</p>`;
-        });
-        section += elements.join("");
-        section += "</section>";
-  
-        document.getElementsByTagName("main").appendChild(section);
+    buildAlert() {
+      // this.data is an array of objects
+      let section = document.createElement("section")
+      let elements = "";
+      for (let i = 0; i < this.data.length; i++){
+        elements += `<p style="padding:5px;color:${this.data[i].color};background-color:${this.data[i].background};">${this.data[i].message}</p>`
       }
+      section.innerHTML = elements;
+      section.classList.add("alert");
+      let main = document.querySelector("main");
+      main.appendChild(section);
+
     }
   }
+  let alert = new Alert();
+  alert.init();
+  alert.buildAlert();
---
+++ src/json/alerts.js
@@ -0,0 +1,8 @@
+export default [
+    {
+        "name":"Test Alert",
+        "message":"Alert!",
+        "background":"#880000",
+        "color":"#000000"
+    }
+]
