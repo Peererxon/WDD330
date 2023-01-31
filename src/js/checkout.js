3fb8f46fdcfbce574ca007d30516511dc22b623f
Joe Burner <113367122+mjburn0428@users.noreply.github.com>
2023-01-31 09:32:19 -0700

    Checkout.js upload

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -1,13 +1,66 @@
-import { loadHeaderFooter } from "./utils.mjs";
-import CheckoutProcess from "./checkoutProcess.js";
-
-loadHeaderFooter();
-const myCheckout = new CheckoutProcess("so-cart", ".checkout-summary");
-myCheckout.init();
-
-// this is how it would look if we listen for the submit on the form
-document.forms["checkout"].addEventListener("submit", (e) => {
+import {
+  getCartTotals,
+  getLocalStorage,
+  qs,
+  loadHeaderFooter,
+  alertMessage,
+} from "./utils.js";
+import ExternalServices from "./externalServices.js";
+let cartItems = getLocalStorage("so-cart");
+let totalQuantity = 0; 
+for (let i = 0; i < cartItems.length; i++) {
+  totalQuantity = totalQuantity + cartItems[i].Quantity;
+}
+let subtotal = getCartTotals(cartItems);
+let tax = 0.06 * subtotal;
+let shipping = 10 + (totalQuantity - 1) * 2;
+let total = subtotal + tax + shipping;
+const services = new ExternalServices();
+function print_checkout() {
+  qs(
+    ".checkoutsub"
+  ).innerHTML = `Item Subtotal(${totalQuantity}):   $${subtotal}`;
+  qs(".tax").innerHTML = `Tax: $${tax.toFixed(2)}`;
+  qs(".shipping").innerHTML = `Shipping Estimate: $${shipping}`;
+  qs(".orderTotal").innerHTML = `<b>Order Total</b>: ${total.toFixed(2)} `;
+}
+async function handleSubmit(e) {
+  // we don't want the form to reload the page...so we call preventDefault
   e.preventDefault();
-  // e.target would contain our form in this case
-  myCheckout.checkout();
-});
+  var myForm = e.target;
+  // convert the form into a js object.
+  const data = formDataToJSON(myForm);
+  data.orderTotal = total;
+  data.shipping = shipping;
+  data.tax = tax;
+  var newCart = cartItems.map((item) => ({
+    Id: item.Id,
+    Name: item.Name,
+    Price: item.FinalPrice,
+  }));
+  data.cart = newCart;
+  data.orderDate = new Date();
+  data.expiration = `${data.expireMM}/${data.expireYY}`;
+  try {
+    const res = await services.checkout(data);
+    window.location.replace("../checkout/checkedOut.html");
+    localStorage.clear();
+    console.log(res);
+  } catch (err) {
+    await console.log(err.message)
+    //display the error to the user
+  }
+
+}
+function formDataToJSON(formElement) {
+  let formData = new FormData(formElement);
+  // Object.fromEntries creates a new object made from an iterable list like an Array or Map
+  // Object.entries takes an object and converts it into an Array that is iterable.
+  const converted = Object.fromEntries(formData.entries());
+  return converted;
+}
+loadHeaderFooter();
+print_checkout();
+document
+  .querySelector("#checkoutForm")
+  .addEventListener("submit", handleSubmit);
