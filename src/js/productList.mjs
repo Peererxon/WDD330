9c281fe3f104fb84ae0ff9cdfadbdc6b04790aab
turtlesboulder <lturtlesrock@gmail.com>
2023-02-25 17:43:34 -0600

    Filter function

--- src/js/productList.mjs
+++ src/js/productList.mjs
@@ -24,13 +24,11 @@ export default class productList {
         this.url = document.URL;
     }
 
-    async init() {
-        const productList = await this.dataSource.getData(this.category);
-        
-
+    async init(options = {}) {
+      // See filterProductList for a specification of the options object
+        let productList = await this.dataSource.getData(this.category);
+        productList = this.filterProductList(productList, options);
         this.renderList(productList);
-      
-
         document.querySelector(".title").innerHTML = this.category[0].toUpperCase() + this.category.substring(1)
         
         //Code for breadcrumbs (product quantity in the list)
@@ -46,11 +44,61 @@ export default class productList {
         renderListWithTemplate(productCardTemplate, this.listElement, productList)
     }
 
-    filterProductList(list) {
-      // return random list of product whose index is less than 4 
-      // from the list of all tents/products.
-      var randomNum = Math.floor(Math.random() * ((list.length + 1) - 4));
-      return list.slice(randomNum, randomNum + 4);
+    filterProductList(list, options) {
+      /* All options are whitelist style, that is everything is excluded but the options with property, if property exists
+      options:
+      {
+        Brand: BrandID. any product whose Brand whose .Id matches this.
+        Color: ColorCode. any product whose has a color whose ColorCode is an exact match to this
+        PriceMinimum: float. any product whose price is greater than or equal to PriceMinimum
+        PriceMaximum: float. any product whose price is less than or equal to PriceMaximum
+        IsClearance: boolean. any product whose IsClearance matches IsClearance
+        IsNew: boolean. any product whose IsNew matches IsNew
+        IsFamousBrand: boolean. any product whose IsFamousBrand matches IsFamousBrand
+      }
+      This will not filter items if that property doesn't exist
+       */
+      if (options.Brand != null){
+        list.filter((item)=>{
+          item.Brand.Id == options.Brand;
+        })
+      }
+      if (options.Color != null){
+        list.filter((item)=>{
+          for (let color of item.Colors){
+            if (color.ColorCode == options.Color){
+              return true;
+            }
+          }
+          return false;
+        })
+      }
+      if (options.PriceMinimum != null){
+        list.filter((item)=>{
+          item.FinalPrice >= options.PriceMinimum
+        })
+      }
+      if (options.PriceMaximum != null){
+        list.filter((item)=>{
+          item.FinalPrice <= options.PriceMaximum
+        })
+      }
+      if (options.IsClearance != null){
+        list.filter((item)=>{
+          item.IsClearance == options.IsClearance
+        })
+      }
+      if (options.IsNew != null){
+        list.filter((item)=>{
+          item.IsNew == options.IsNew
+        })
+      }
+      if (options.FamousBrand != null){
+        list.filter((item)=>{
+          item.IsFamousBrand == options.IsFamousBrand
+        })
+      }
+      return list;
     }
 }
 
