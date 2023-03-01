2a3faa180ae2104e0c292be4f028cdb212bb345a
anderson gil <andersongil.dev@gmail.com>
2023-03-01 16:24:08 -0400

    formatt file

--- src/js/admin.js
+++ src/js/admin.js
@@ -1,14 +1,14 @@
-import auth from './admin.mjs'
-function fixButton(){
-    let button = document.querySelector('#submit');
-    button.addEventListener('submit',(e)=>{
-        e.preventDefault();
-        auth.login();
-    })
+import auth from './admin.mjs';
+function fixButton() {
+  let button = document.querySelector('#submit');
+  button.addEventListener('submit', (e) => {
+    e.preventDefault();
+    auth.login();
+  });
 }
-function addForm(){
-    let main = document.querySelector('main');
-    main.innerHTML = auth.showLogin();
+function addForm() {
+  let main = document.querySelector('main');
+  main.innerHTML = auth.showLogin();
 }
 addForm();
 fixButton();
