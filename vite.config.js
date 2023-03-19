b709cda07065e997eacb548060f37a660487227b
anderson gil <andersongil.dev@gmail.com>
2023-03-19 14:48:06 -0400

    fix deploy error
    Error: Could not resolve entry module (src/product-listing/product-search.html).

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,24 @@
-import { resolve } from 'path';
-import { defineConfig } from 'vite';
+import { resolve } from "path";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  root: 'src/',
+  root: "src/",
 
   build: {
-    outDir: '../dist',
+    outDir: "../dist",
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'src/index.html'),
-        cart: resolve(__dirname, 'src/cart/index.html'),
-        checkout: resolve(__dirname, 'src/checkout/index.html'),
-        success: resolve(__dirname, 'src/checkout/success.html'),
-        product: resolve(__dirname, 'src/product_pages/index.html'),
-        directory: resolve(__dirname, 'src/product-listing/index.html'),
-        search: resolve(__dirname, 'src/product-listing/product-search.html'),
-        admin: resolve(__dirname, 'src/admin/index.html'),
-        wishlist: resolve(__dirname, 'src/cart/wishlist.html'),
-        registration: resolve(__dirname, 'src/registration/index.html')
-      }
-    }
-  }
+        main: resolve(__dirname, "src/index.html"),
+        cart: resolve(__dirname, "src/cart/index.html"),
+        checkout: resolve(__dirname, "src/checkout/index.html"),
+        success: resolve(__dirname, "src/checkout/success.html"),
+        product: resolve(__dirname, "src/product_pages/index.html"),
+        directory: resolve(__dirname, "src/product-listing/index.html"),
+        search: resolve(__dirname, "src/product-listing/products-search.html"),
+        admin: resolve(__dirname, "src/admin/index.html"),
+        wishlist: resolve(__dirname, "src/cart/wishlist.html"),
+        registration: resolve(__dirname, "src/registration/index.html"),
+      },
+    },
+  },
 });
