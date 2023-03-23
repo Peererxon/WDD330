74b181b5e10c5f5bc3e851f7586fe2742e38b380
anderson gil <andersongil.dev@gmail.com>
2023-03-23 15:03:00 -0400

    delete debuggers

--- src/js/wishlist.js
+++ src/js/wishlist.js
@@ -1,23 +1,26 @@
 import ShoppingCart from "./shoppingCart.mjs";
-import { getLocalStorage, setLocalStorage, showCartQuantity} from "./utils.mjs";
+import {
+  getLocalStorage,
+  setLocalStorage,
+  showCartQuantity,
+} from "./utils.mjs";
 import ProductDetails from "./ProductDetails.mjs";
 
 const cart = new ShoppingCart("so-cart", ".product-list");
-cart.renderCartContents('wishlist');
+cart.renderCartContents("wishlist");
 
-document.querySelector("#check-out").addEventListener("click", ()=>{
-    debugger;
-    if (document.querySelector(".product-list").children.length == 0){
-        alert("You cannot add nothing to the cart!");
-    }else{
-        let details = new ProductDetails("", "");
-        let wishlist = getLocalStorage('wishlist');
-        wishlist.forEach((item)=>{
-            details.addProductToCart(item, 'so-cart')
-        })
-    }
+document.querySelector("#check-out").addEventListener("click", () => {
+  if (document.querySelector(".product-list").children.length == 0) {
+    alert("You cannot add nothing to the cart!");
+  } else {
+    let details = new ProductDetails("", "");
+    let wishlist = getLocalStorage("wishlist");
+    wishlist.forEach((item) => {
+      details.addProductToCart(item, "so-cart");
+    });
+  }
 
-    setLocalStorage('wishlist',[]);
-    cart.renderCartContents('wishlist');
-    showCartQuantity();
-})
+  setLocalStorage("wishlist", []);
+  cart.renderCartContents("wishlist");
+  showCartQuantity();
+});
